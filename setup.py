76d5df73013dfb16ba58b6766f90d9bdd6cf063c
Donearm <forod.g@gmail.com>
2011-12-15 19:50:54 +0100

    initial skeleton of setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+###############################################################################
+# Copyright (c) 2011, Gianluca Fiore
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+###############################################################################
+
+from distutils.core import setup
+
+setup(name='Cryptanddrop',
+        version='0.1',
+        author='Gianluca Fiore',
+        author_email='forod.g@gmail.com',
+        url='https://github.com/Donearm/CryptandDrop',
+        download_url='https://github.com/Donearm/CryptandDrop',
+        description='An encrypter/decrypter for files hosted on Dropbox',
+        long_description=open('README.mdown').read(),
+        packages=['cryptanddrop'],
+        provides=['cryptanddrop'],
+        requires=['pycrypto', 'python-oauth', 'simplejson'],
+        keywords='encrypter decrypter dropbox',
+        license='COPYING',
+        classfiers=[],
+        )
